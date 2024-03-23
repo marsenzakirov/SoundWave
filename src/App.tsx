8415bc89051a46b0887416710222e4c26c153204
Marsen <marsenzakirov11@gmail.com>
2024-03-23 01:32:51 -0500

    feat: change theme to dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/header';
-import Footer from './components/footer';
 import Home from './pages/home';
 import Artist from './pages/artist';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+
+
+
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/artist" element={<Artist />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <ThemeProvider theme={darkTheme}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/artist" element={<Artist />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+
   );
 }
 
